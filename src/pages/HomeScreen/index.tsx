dafa17ea267ded85b8a5c2069520ca32f151e2fc
mta-sistemas <andremurilovf@gmail.com>
2025-03-14 16:37:29 -0300

    TESTE

--- src/pages/HomeScreen/index.tsx
+++ src/pages/HomeScreen/index.tsx
@@ -25,7 +25,7 @@ function HomeScreen() {
     return (
         <Container>
             <AnimatedBackground />
-            <Title>Seja bem vindo ao sistema MTA...</Title>
+            <Title>Seja bem vindo ao sistema LUIZAO...</Title>
             <div style={{ height: 20 }} />
             <motion.div
                 initial={{ opacity: 0, y: 50 }}
