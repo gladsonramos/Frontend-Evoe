3259b08afd6ac2a6b2f3f52e05bbdce25a77e9de
mta-sistemas <andremurilovf@gmail.com>
2025-03-14 15:15:07 -0300

    oi

--- src/pages/HomeScreen/index.tsx
+++ src/pages/HomeScreen/index.tsx
@@ -25,7 +25,7 @@ function HomeScreen() {
     return (
         <Container>
             <AnimatedBackground />
-            <Title>Seja bem vindo ao sistema de cadastro Evoé...</Title>
+            <Title>Seja bem vindo ao sistema de cadastro Evoé 2...</Title>
             <div style={{ height: 20 }} />
             <motion.div
                 initial={{ opacity: 0, y: 50 }}
